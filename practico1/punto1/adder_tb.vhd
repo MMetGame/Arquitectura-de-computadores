d4bc05c592889406f79522a5be5e1ad0709aeb0f
dario molina <darvak8@gmail.com>
2014-08-20 01:42:25 -0300

    viendo que onda el tb , no anda :(

--- practico1/punto1/adder_tb.vhd
+++ practico1/punto1/adder_tb.vhd
@@ -9,20 +9,22 @@ end entity;
 --arquitectura
 architecture test_bench of adder_tb is
     --declaracion del componente que sera instanciado
-    component adder
+    component sum
     port(
         a,b: in std_logic_vector(31 downto 0);
         y: out std_logic_vector(31 downto 0)
     );
     end component;
-    signal a, b, y: std_logic_vector(31 downto 0);
+    signal a_tb, b_tb, y_tb: std_logic_vector(31 downto 0);
 begin
-    process (a,b)
+    process
     begin
-    u0: adder port map (a, b, y);
-
-    a <= x"00000007" after 0 fs, x"00000014" after 5 fs, x"00000001" after 10 fs;
-    b <= x"FF000000" after 0 fs, x"EEFF0010" after 5 fs, x"FFFFFFFF" after 10 fs;
+    a_tb <= a;
+    b_tb <= b;
+    y_tb <= y;
+    u0: sum port map (a_tb, b_tb, y_tb);
+    a_tb <= x"00000007" after 0 fs, x"00000014" after 5 fs, x"00000001" after 10 fs;
+    b_tb <= x"FF000000" after 0 fs, x"EEFF0010" after 5 fs, x"FFFFFFFF" after 10 fs;
     end process;
 end architecture;
 
