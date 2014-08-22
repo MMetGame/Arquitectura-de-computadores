c1dfb725eb6df8dd997194456f672f74a59516ac
dario molina <darvak8@gmail.com>
2014-08-22 02:27:30 -0300

    implementando desplazador

--- practico1/punto4/desplazador.vhd
+++ practico1/punto4/desplazador.vhd
@@ -12,5 +12,14 @@ end entity;
 
 --arquitectura
 architecture despl_32 of desplazador is begin
-
+    process 
+        variable r: std_logic_vector(31 downto 0);
+    begin
+        r(0) := "0"; --posicion 1
+        r(1) := "0"; --posicion 2
+        for i in 2 to a'LENGTH-1 loop
+            r(i) := a(i+2);
+        end loop;
+    end process;
 end architecture;
+-- preguntar big endian o little endian???
